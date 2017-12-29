bb3b1c4c59f8ac0fdf67d486081792b65263a42b
raheel <raheel@mobinspire.com>
2017-12-29 18:42:05 +0500

    - Instabug Lib version updated - removed unused code

--- app/src/main/java/com/bykea/pk/partner/ui/activities/BookingActivity.java
+++ app/src/main/java/com/bykea/pk/partner/ui/activities/BookingActivity.java
@@ -599,7 +599,7 @@ public class BookingActivity extends BaseActivity implements GoogleApiClient.OnC
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
             case 13:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
