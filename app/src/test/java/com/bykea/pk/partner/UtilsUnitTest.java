299c51215f3c0e4af7e54836495899a37bfda443
raheel <raheel@mobinspire.com>
2018-08-13 11:13:58 +0500

    - Few Test case methods added

--- app/src/test/java/com/bykea/pk/partner/UtilsUnitTest.java
+++ app/src/test/java/com/bykea/pk/partner/UtilsUnitTest.java
@@ -2,6 +2,7 @@ package com.bykea.pk.partner;
 
 import android.util.Patterns;
 
+import com.bykea.pk.partner.ui.helpers.AppPreferences;
 import com.bykea.pk.partner.utils.Utils;
 
 import org.apache.commons.lang3.StringUtils;
@@ -84,6 +85,21 @@ public class UtilsUnitTest {
         assertTrue(convertedNo.split("\\.")[1].length() <= 2);
         convertedNo = Utils.upto2decimalPlaces(129.);
         assertTrue(convertedNo.split("\\.")[1].length() <= 2);
+    }
+
+    /**
+     * Test method to convert double to 2 decimal points
+     * This test case will be considered correct/pass when any double no will have 2 or less digits after point
+     */
+
+    @Test
+    public void calculateDistance() {
+        String distance = Utils.calculateDistanceInKm(33.6844, 73.0479,
+                33.6844, 73.0479);
+        assertTrue(StringUtils.isNotBlank(distance));
+        distance = Utils.calculateDistanceInKm(24.8607, 67.0011,
+                33.6844, 73.0479);
+        assertTrue(StringUtils.isNotBlank(distance));
     }
 
 }
