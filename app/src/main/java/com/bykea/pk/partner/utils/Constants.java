3202f3fc13fcd8347ca3bd36c6cbf109da6104db
fazalBykea <fazal.hussain@bykea.com>
2018-10-04 19:11:34 +0500

    BS-1413-Map Data to UI
    Signed-off-by: fazalBykea <fazal.hussain@bykea.com>

--- app/src/main/java/com/bykea/pk/partner/utils/Constants.java
+++ app/src/main/java/com/bykea/pk/partner/utils/Constants.java
@@ -167,4 +167,43 @@ public class Constants {
         public final static String CONVERSION_ERROR = "ConversionError ";
         public final static String OTHER_ERROR = "Other Error ";
     }
+
+    /***
+     * This inner class will contain Constants for Trip Types
+     */
+    public static class TripTypes {
+
+        public static final String RIDE_TYPE = "Ride";
+
+        public static final String CLASSIFIED_TYPE = "Classifieds";
+
+        public static final String MOVIETICKET_TYPE = "Movie Ticket";
+
+        public static final String AIRTICKET_TYPE = "Air Ticket";
+
+        public static final String PURCHASE_TYPE = "Purchase";
+        public static final String PURCHASE_NAME = "Bring";
+
+        public static final String DELIVERY_TYPE = "Delivery";
+
+        public static final String DEPOSIT_TYPE = "Deposit";
+
+        public static final String TOPUP_TYPE = "Top-Up";
+
+        public static final String BUSTICKET_TYPE = "Bus Ticket";
+
+        public static final String VAN_TYPE = "Carry Van";
+        public static final String COURIER_TYPE = "Courier";
+
+        public static final String INSURANCE_TYPE = "Bima";
+        public static final String INSURANCE_NAME = "Insurance";
+
+        public static final String JOBS_TYPE = "Jobs";
+
+        public static final String FOOD_TYPE = "Food";
+        public static final String FOOD_BOOKING_TYPE = "MB";
+
+        public static final String BILL_TYPE = "Utility Bill";
+
+    }
 }
