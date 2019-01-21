eb91133c309ac9d32591c8397cc790436aba01c5
amirraza19 <amir.raza@bykea.com>
2019-01-21 11:20:35 +0500

    Handle making calls to drop off location's person Call, option can only be enabled when ride has started or arrived BS-1935-After-Started-Call-Icon-Should-Be-Of-Green-Color

--- app/src/main/java/com/bykea/pk/partner/utils/Constants.java
+++ app/src/main/java/com/bykea/pk/partner/utils/Constants.java
@@ -28,6 +28,11 @@ public class Constants {
     public static final String TAG_LOCATION = "LocationUpdate";
     public static final String TAG_GOOGLE_MAP = "GOOGLE MAP TRACKING";
 
+    /*Added these ride statuses to handle making calls to drop off location's person
+    * Call option can only be enabled when ride has started or arrived*/
+    public static final String RIDE_STARTED = "Started";
+    public static final String RIDE_ARRIVED = "Arrived";
+
     public static final int RIDE_ACCEPTANCE_TIMEOUT = 20800;
     public static final float TIME_IN_MILLISECONDS_PERCENTAGE = 5f;
 
