24cf7a741ffbf7e75d6724d1026bcd777d0983ad
Yousuf Sohail <yousuf.sohail@bykea.com>
2019-04-09 18:15:09 +0500

    Custom fonts and text view configured.

---
+++ app/src/main/java/com/bykea/pk/partner/ui/helpers/Fonts.java
@@ -0,0 +1,32 @@
+package com.bykea.pk.partner.ui.helpers;
+
+public enum Fonts {
+    Open_Sans_Regular("open_sans_regular.ttf"),
+    Roboto_Italic("roboto_italic.ttf"),
+    Roboto_light("roboto_light.ttf"),
+    Roboto_Light_Italic("roboto_light_italic.ttf"),
+    Roboto_Medium("roboto_medium.ttf"),
+    Roboto_Medium_Italic("roboto_medium_italic.ttf"),
+    Roboto_Regular("roboto_regular.ttf"),
+    Roboto_Thin("roboto_thin.ttf"),
+    Roboto_Thin_Italic("roboto_thin_italic.ttf"),
+    Signika_Regular("signika_regular.ttf"),
+    Museo_Sans("MuseoSans_1.otf"),
+    Roboto_Bold("roboto_bold.ttf"),
+    Open_Sans_Light("open_sans_light.ttf"),
+    Jameel_Noori_Nastaleeq("jameel_noori_nastaleeq.ttf"),
+    Open_Sans_Bold("open_sans_bold.ttf"),
+    Open_Sans_Semi_Bold("open_sans_semi_bold.ttf"),
+    Raleway_Semibold("raleway_semibold.ttf"),
+    Pak_Nastaleeq_Urdu("pak_nastaleeq_urdu.ttf");
+
+    private final String name;
+
+    Fonts(String s) {
+        name = s;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
