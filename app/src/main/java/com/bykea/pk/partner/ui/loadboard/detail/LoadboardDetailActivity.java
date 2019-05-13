a72f918b96b7d6f516ed663fb697d8cd524bc938
Yousuf Sohail <yousuf.sohail@bykea.com>
2019-05-13 14:55:07 +0500

    Loadboard Detail screen activity and fragment added.

---
+++ app/src/main/java/com/bykea/pk/partner/ui/loadboard/detail/LoadboardDetailActivity.java
@@ -0,0 +1,93 @@
+package com.bykea.pk.partner.ui.loadboard.detail;
+
+import android.os.Bundle;
+import android.support.v7.widget.AppCompatImageView;
+import android.view.View;
+
+import com.bykea.pk.partner.R;
+import com.bykea.pk.partner.models.response.LoadboardBookingDetailResponse;
+import com.bykea.pk.partner.repositories.UserDataHandler;
+import com.bykea.pk.partner.repositories.UserRepository;
+import com.bykea.pk.partner.ui.activities.BaseActivity;
+import com.bykea.pk.partner.utils.Dialogs;
+import com.bykea.pk.partner.utils.HTTPStatus;
+import com.bykea.pk.partner.utils.Utils;
+import com.bykea.pk.partner.widgets.FontTextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Loadboard booking detail screen ACTIVITY - opening from homeScreen's loadboard listing items
+ * this activity will hold booking detail fragment
+ */
+public class LoadboardDetailActivity extends BaseActivity {
+
+    private LoadboardDetailActivity mCurrentActivity;
+    private UserRepository mRepository;
+
+    public static String BOOKING_ID = "BOOKING_ID";
+
+    @BindView(R.id.backBtn)
+    AppCompatImageView backBtn;
+    @BindView(R.id.bookingTypeIV)
+    AppCompatImageView bookingTypeIV;
+    @BindView(R.id.bookingNoTV)
+    FontTextView bookingNoTV;
+
+    private String bookingId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loadboard_detail);
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+        mCurrentActivity = this;
+        bookingId = getIntent().getStringExtra(BOOKING_ID);
+        ButterKnife.bind(this);
+
+        initViews();
+        initListeners();
+    }
+
+    /**
+     * initialize views and objects related to this screen
+     */
+    private void initViews() {
+        mRepository = new UserRepository();
+        Dialogs.INSTANCE.showLoader(mCurrentActivity);
+        mRepository.loadboardBookingDetail(mCurrentActivity, bookingId, new UserDataHandler() {
+            @Override
+            public void onLoadboardBookingDetailResponse(LoadboardBookingDetailResponse response) {
+                Dialogs.INSTANCE.dismissDialog();
+                bookingNoTV.setText(response.getData().getOrderNo());
+//                bookingTypeIV.setImageResource();
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.bookingDetailContainerFL, LoadboardDetailFragment.newInstance(response.getData()))
+                        .commitAllowingStateLoss();
+            }
+
+            @Override
+            public void onError(int errorCode, String errorMessage) {
+                Dialogs.INSTANCE.dismissDialog();
+                if (errorCode == HTTPStatus.UNAUTHORIZED) {
+                    Utils.onUnauthorized(mCurrentActivity);
+                } else {
+                    Dialogs.INSTANCE.showToast(mCurrentActivity, errorMessage);
+                }
+            }
+        });
+    }
+
+    /**
+     * initialize click listeners for this screen's button or widgets
+     */
+    private void initListeners() {
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
