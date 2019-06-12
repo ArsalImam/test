9c7a6365120b9efc82985c59caa51bc2147ea0d6
Yousuf Sohail <yousuf.sohail@bykea.com>
2019-06-12 16:10:54 +0500

    Binding for service icons.

--- app/src/main/java/com/bykea/pk/partner/ui/loadboard/common/BindingAdapters.kt
+++ app/src/main/java/com/bykea/pk/partner/ui/loadboard/common/BindingAdapters.kt
@@ -1,6 +1,7 @@
 package com.bykea.pk.partner.ui.loadboard.common
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.ListView
 import androidx.databinding.BindingAdapter
 import com.bykea.pk.partner.dal.Booking
@@ -19,6 +20,13 @@ object BindingAdapters {
         with(listView.adapter as BookingsAdapter) {
             replaceData(items)
         }
+    }
+
+    @BindingAdapter("app:srcName")
+    fun setImageViewResource(imageView: ImageView, resource: String) {
+        val id = imageView.context.resources.getIdentifier(resource, "drawable", imageView.context.packageName)
+        val drawable = imageView.context.resources.getDrawable(id)
+        imageView.setImageDrawable(drawable)
     }
 
     @BindingAdapter("app:goneUnless")
