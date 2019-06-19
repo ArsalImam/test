7db66170e4693017643e8612d8f64752a17af0cb
smsrn <sibtain.raza@bykea.com>
2019-06-19 12:28:48 +0500

    Java doc add.

--- app/src/main/java/com/bykea/pk/partner/ui/helpers/StringCallBack.java
+++ app/src/main/java/com/bykea/pk/partner/ui/helpers/StringCallBack.java
@@ -1,5 +1,11 @@
 package com.bykea.pk.partner.ui.helpers;
 
+/**
+ * TWO CALLBACKS
+ * SET TO DEFAULT AS INTERFACE DO NOT ENFORCE TO IMPLEMENT ALL THE METHODS
+ * ONLY IMPLEMENT THE REQUIRED ONE
+ */
+
 public interface StringCallBack {
     default void onCallBack(String msg){}
     default void onCallBack(String msg,String msg1){}
