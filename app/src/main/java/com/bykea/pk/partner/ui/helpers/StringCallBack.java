5dc8b8ec1f93817c0bf0cd07dd1599826344635a
smsrn <sibtain.raza@bykea.com>
2019-06-21 11:07:08 +0500

    Added Java Doc.

--- app/src/main/java/com/bykea/pk/partner/ui/helpers/StringCallBack.java
+++ app/src/main/java/com/bykea/pk/partner/ui/helpers/StringCallBack.java
@@ -7,6 +7,16 @@ package com.bykea.pk.partner.ui.helpers;
  */
 
 public interface StringCallBack {
+    /**
+     * Use If Need One String Callback
+     * @param msg : Callback return for the first string
+     */
     default void onCallBack(String msg){}
+
+    /**
+     * Use If Need Two Strings Callback
+     * @param msg : Callback return for the first string
+     * @param msg1 : Callback return for the second string
+     */
     default void onCallBack(String msg,String msg1){}
 }
--- dal/src/main/java/com/bykea/pk/partner/dal/util/Const.kt
+++ dal/src/main/java/com/bykea/pk/partner/dal/util/Const.kt
@@ -8,4 +8,4 @@ val BUILD_VARIANT_LOCAL_FLAVOR = "local"
 const val DRIVER_ID = "DRIVER_ID"
 const val ACCESS_TOKEN = "ACCESS_TOKEN"
 const val EMPTY_STRING = ""
-const val SERVICE_CODE_SEND = 21
+const val SERVICE_CODE_SEND = 21s
