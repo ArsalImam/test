a1b64463ecb54fbe21d1a8a8e56903e29a825e9f
smsrn <sibtain.raza@bykea.com>
2019-07-20 16:37:05 +0500

    Add screen for Zendesk.

--- app/src/main/java/com/bykea/pk/partner/ui/helpers/ActivityStackManager.java
+++ app/src/main/java/com/bykea/pk/partner/ui/helpers/ActivityStackManager.java
@@ -42,6 +42,7 @@ import com.bykea.pk.partner.ui.activities.RegistrationActivity;
 import com.bykea.pk.partner.ui.activities.SavePlaceActivity;
 import com.bykea.pk.partner.ui.activities.ShahkarActivity;
 import com.bykea.pk.partner.ui.loadboard.detail.JobRequestDetailActivity;
+import com.bykea.pk.partner.ui.support.ComplainZendeskIdentityActivity;
 import com.bykea.pk.partner.ui.support.ComplaintListActivity;
 import com.bykea.pk.partner.ui.support.ComplaintSubmissionActivity;
 import com.bykea.pk.partner.utils.Constants;
@@ -444,6 +445,16 @@ public class ActivityStackManager {
             intent.putExtra(INTENT_TRIP_HISTORY_DATA, tripHistoryData);
         context.startActivity(intent);
     }
+
+    /**
+     * Screen for waiting, to get zendesk sdk setup.
+     * @param context : Calling Activity
+     */
+    public void startZendeskIdentityActivity(Context context) {
+        Intent intent = new Intent(context, ComplainZendeskIdentityActivity.class);
+        context.startActivity(intent);
+    }
+
     /**
      * This method will start details activity for Scheduled Delivery Service
      *
@@ -510,6 +521,7 @@ public class ActivityStackManager {
 
     /**
      * Submitted Tickets Activity
+     *
      * @param context Calling Activity
      */
     public void startComplainListActivity(Context context) {
