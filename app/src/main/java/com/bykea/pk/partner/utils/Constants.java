4c0e8ad9941b421487e6df55e7b91646dcf177af
Yousuf Sohail <yousuf.sohail@bykea.com>
2019-11-14 19:26:24 +0500

    Change distance matrix api call interval to 300 seconds during started state

--- app/src/main/java/com/bykea/pk/partner/utils/Constants.java
+++ app/src/main/java/com/bykea/pk/partner/utils/Constants.java
@@ -661,4 +661,5 @@ public class Constants {
 
     public final static int DIRECTION_API_MIX_THRESHOLD_METERS = 45; //meters
     public final static int DISTANCE_MATRIX_API_CALL_THRESHOLD_TIME = 7; //70 seconds
+    public final static int DISTANCE_MATRIX_API_CALL_START_STATE_THRESHOLD_TIME = 30; //300 seconds
 }
