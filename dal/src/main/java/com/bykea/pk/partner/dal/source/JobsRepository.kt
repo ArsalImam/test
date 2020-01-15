f586f669ab6869d25a802590f5f73563b2b3e3ad
smsrn <sibtain.raza@bykea.com>
2020-01-15 13:01:53 +0500

    Make end address nullable.

--- dal/src/main/java/com/bykea/pk/partner/dal/source/JobsRepository.kt
+++ dal/src/main/java/com/bykea/pk/partner/dal/source/JobsRepository.kt
@@ -144,7 +144,7 @@ class JobsRepository(
         jobsRemoteDataSource.cancelJob(jobId, AppPref.getDriverId(pref), AppPref.getAccessToken(pref), AppPref.getLat(pref), AppPref.getLng(pref), reason, callback)
     }
 
-    override fun finishJob(jobId: String, route: ArrayList<LocCoordinatesInTrip>, endAddress: String, callback: JobsDataSource.FinishJobCallback) {
+    override fun finishJob(jobId: String, route: ArrayList<LocCoordinatesInTrip>, endAddress: String?, callback: JobsDataSource.FinishJobCallback) {
         val body = FinishJobRequest(AppPref.getDriverId(pref), AppPref.getAccessToken(pref), AppPref.getLat(pref), AppPref.getLng(pref), endAddress, route)
         jobsRemoteDataSource.finishJob(jobId, body, callback)
     }
