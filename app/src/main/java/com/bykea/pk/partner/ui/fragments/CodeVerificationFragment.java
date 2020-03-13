29bc506dfdd213685174402852a38ba0e6fda770
smsrn <sibtain.raza@bykea.com>
2020-03-13 09:47:39 +0500

    1) Instead of getString directly used application context to get. 2) Handle empty check for login response message.

--- app/src/main/java/com/bykea/pk/partner/ui/fragments/CodeVerificationFragment.java
+++ app/src/main/java/com/bykea/pk/partner/ui/fragments/CodeVerificationFragment.java
@@ -366,7 +366,7 @@ public class CodeVerificationFragment extends Fragment implements GoogleApiClien
      */
     private void unRegisterSMSReceiver() {
         try {
-            if (mCurrentActivity != null && smsBroadcastReceiver!=null) {
+            if (mCurrentActivity != null && smsBroadcastReceiver != null) {
                 mCurrentActivity.unregisterReceiver(smsBroadcastReceiver);
             }
         } catch (Exception ex) {
@@ -506,8 +506,9 @@ public class CodeVerificationFragment extends Fragment implements GoogleApiClien
     private void handleDriverErrorCase(LoginResponse loginResponse) {
         if (loginResponse != null) {
             String errorMessage;
-            if (StringUtils.containsIgnoreCase(loginResponse.getMessage(),
-                    getString(R.string.invalid_code_error_message))) {
+            String invalidErrorMessage = DriverApp.getContext().getString(R.string.invalid_code_error_message);
+            if (StringUtils.isNotEmpty(loginResponse.getMessage()) &&
+                    StringUtils.containsIgnoreCase(loginResponse.getMessage(), invalidErrorMessage)) {
                 errorMessage = getString(R.string.invalid_phone_urdu);
             } else {
                 errorMessage = loginResponse.getMessage();
