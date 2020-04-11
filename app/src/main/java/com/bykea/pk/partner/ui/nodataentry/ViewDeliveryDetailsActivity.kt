6a4009b930e0cf6da84b80f6ebbd2f9fb48f4460
smsrn <sibtain.raza@bykea.com>
2020-04-12 00:45:32 +0500

    1) Add toolbar in view delivery details layout. 2) Change made in strings.xml.

--- app/src/main/java/com/bykea/pk/partner/ui/nodataentry/ViewDeliveryDetailsActivity.kt
+++ app/src/main/java/com/bykea/pk/partner/ui/nodataentry/ViewDeliveryDetailsActivity.kt
@@ -1,10 +1,13 @@
 package com.bykea.pk.partner.ui.nodataentry
 
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.bykea.pk.partner.R
 import com.bykea.pk.partner.databinding.ActivityViewDeliveryDetailsBinding
 import com.bykea.pk.partner.ui.activities.BaseActivity
+import com.zendesk.util.StringUtils
+import kotlinx.android.synthetic.main.custom_toolbar.*
 
 class ViewDeliveryDetailsActivity : BaseActivity() {
     lateinit var binding: ActivityViewDeliveryDetailsBinding
@@ -12,5 +15,9 @@ class ViewDeliveryDetailsActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_view_delivery_details)
+
+        setTitleCustomToolbarWithUrdu("KHILHQY7", StringUtils.EMPTY_STRING)
+        fLLocation.visibility = View.VISIBLE
+        tVLocationAlphabet.text = "A"
     }
 }
