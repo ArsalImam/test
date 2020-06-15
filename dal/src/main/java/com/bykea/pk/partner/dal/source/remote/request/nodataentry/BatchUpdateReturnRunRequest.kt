5df35730aa49f91e79da40f7a77f7fc29f45d1f7
smsrn <sibtain.raza@bykea.com>
2020-06-15 19:05:45 +0500

    Remove email address from java doc.

--- dal/src/main/java/com/bykea/pk/partner/dal/source/remote/request/nodataentry/BatchUpdateReturnRunRequest.kt
+++ dal/src/main/java/com/bykea/pk/partner/dal/source/remote/request/nodataentry/BatchUpdateReturnRunRequest.kt
@@ -3,6 +3,5 @@ package com.bykea.pk.partner.dal.source.remote.request.nodataentry
 
 /**
  * Created by Sibtain Raza on 5/18/2020.
- * smsibtainrn@gmail.com
  */
 data class BatchUpdateReturnRunRequest(var return_run: Boolean? = null)
