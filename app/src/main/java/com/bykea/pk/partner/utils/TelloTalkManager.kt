8ada9658b87c8c5c701a9f3d6f050d8e586f1221
Arsal Imam <arsal.imam@bykea.com>
2020-07-13 18:53:03 +0500

    docs added

--- app/src/main/java/com/bykea/pk/partner/utils/TelloTalkManager.kt
+++ app/src/main/java/com/bykea/pk/partner/utils/TelloTalkManager.kt
@@ -5,9 +5,20 @@ import com.bykea.pk.partner.R
 import com.bykea.pk.partner.ui.helpers.AppPreferences
 import com.tilismtech.tellotalksdk.managers.TelloApiClient
 
+/**
+ * this is the utitlity manager class for tellotalk
+ *
+ * [author] ArsalImam
+ */
 class TelloTalkManager {
+    /**
+     * tello client instance
+     */
     private var telloApiClient: TelloApiClient
 
+    /**
+     * constructor to initialize tello sdk object
+     */
     constructor() {
         val builder: TelloApiClient.Builder = TelloApiClient.Builder()
                 .accessKey(Constants.ACCESS_KEY_TELLO_TALK)
@@ -16,12 +27,20 @@ class TelloTalkManager {
         telloApiClient = builder.build()
     }
 
+    /**
+     * this will configure tello fcm client
+     */
     fun setupFcm() {
         telloApiClient.let {
             if (it.isLoggedIn) it.updateFcmToken(AppPreferences.getRegId())
         }
     }
 
+    /**
+     * this will perform login to tello talk instance
+     *
+     * [callback] this will return controll from tello to our app
+     */
     fun performLogin(callback: (isLoggedIn: Boolean) -> Unit) {
         telloApiClient.let {
             it.login { isLoggedInUser ->
@@ -36,18 +55,32 @@ class TelloTalkManager {
         }
     }
 
+    /**
+     * this method will be used to open co-operate chat for CSR
+     *
+     * [activity] context of the activity from which it will open
+     */
     fun openCorporateChat(activity: Activity?) {
         telloApiClient.openCorporateChat(activity)
     }
 
+    /**
+     * this will awake tello's client on fcm received for fcm
+     */
     fun onMessageReceived() {
         if (telloApiClient.isLoggedIn)
             telloApiClient.onMessageNotificationReceived()
     }
 
     companion object {
+        /**
+         * singleton instance of this class
+         */
         private var INSTANCE: TelloTalkManager? = null
 
+        /**
+         * synchronized method to generate singleton instance of this class
+         */
         @JvmStatic
         fun instance(): TelloTalkManager = INSTANCE ?: synchronized(TelloTalkManager::class.java) {
             INSTANCE ?: TelloTalkManager()
