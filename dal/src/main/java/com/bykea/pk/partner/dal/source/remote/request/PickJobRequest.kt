0d36e377ce13e8fd702e3cd8094a842fb9197d66
smsrn <sibtain.raza@bykea.com>
2020-07-28 15:06:38 +0500

    Correction

--- dal/src/main/java/com/bykea/pk/partner/dal/source/remote/request/PickJobRequest.kt
+++ dal/src/main/java/com/bykea/pk/partner/dal/source/remote/request/PickJobRequest.kt
@@ -2,6 +2,5 @@ package com.bykea.pk.partner.dal.source.remote.request
 
 data class PickJobRequest(
         val lat: Double,
-        val lng: Double,
-        val dispatch: Boolean
+        val lng: Double
 )
