de993b7d255e9a52e461de291c9863248212512f
sibtain <sibtain.raza@bykea.com>
2020-09-30 12:21:49 +0500

    Stop Login By Pass If Token Is Null

--- app/src/main/java/com/bykea/pk/partner/communication/rest/RestRequestHandler.java
+++ app/src/main/java/com/bykea/pk/partner/communication/rest/RestRequestHandler.java
@@ -237,7 +237,8 @@ public class RestRequestHandler {
                     }
                 } else {
                     if (response.isSuccessful()) {
-                        if (null != mResponseCallBack) {
+                        if (null != mResponseCallBack && null != response.body().getUser() &&
+                                StringUtils.isNotEmpty(response.body().getUser().getAccessToken())) {
                             mResponseCallBack.onResponse(response.body());
                         }
                     } else {
